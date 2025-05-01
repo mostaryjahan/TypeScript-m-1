91ab4dc5f532e6e1e449c5fcf8719ac8e3ad6860
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-05-01 21:31:24 +0600

    add

--- typeScript/src/1.5.ts
+++ typeScript/src/1.5.ts
@@ -1,1 +1,31 @@
 //reference type ---> object
+
+const user : {
+    company: 'tesla' //type ->literal types
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    isMarried: boolean;
+} = {
+    company: 'tesla',
+   firstName: "mostary",
+   middleName: "jahan",
+   lastName: "brishty",
+   isMarried: false,
+}
+
+// user.company ="ph" it gives error cause literal type
+
+const user1 : {
+    readonly company: string
+    firstName: string;
+    middleName?: string; //optional
+    lastName: string;
+} = {
+   firstName: "mostary",
+   lastName: "brishty",
+   company: "ph"
+}
+
+// user1.company = "oh" error cause readonly
+
