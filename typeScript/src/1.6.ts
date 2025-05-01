0768d47894925b3d27cc91b1293520ad06c8da50
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2025-05-01 22:34:47 +0600

    funtion

---
+++ typeScript/src/1.6.ts
@@ -0,0 +1,37 @@
+//normal function
+// arrow function
+
+//normal function
+function add(num1: number = 10, num2: number = 20): number {
+  return num1 + num2;
+}
+
+add(2, "2"); //type error cause number+ string
+
+// arrow function
+const addArrow = (num1: number, num2: number): number => num1 + num2;
+
+addArrow(2, 2);
+addArrow(2, "2"); //type error cause number+ string
+
+//object--> function --> method
+const rich = {
+  name: "brishty",
+  balance: 0,
+  addBalance(balance: number): number {
+    return this.balance + balance;
+  },
+};
+
+const superRich = {
+  name: "brishty",
+  balance: 0,
+  addBalance(balance: number): string {
+    return `Her new balance is : ${this.balance + balance}`;
+  },
+}; //return will be string because i use here `` string
+
+//callback function
+const array: number[] = [1, 2, 3, 4];
+
+const newArray: number[] = array.map((elem: number): number => elem * elem);
